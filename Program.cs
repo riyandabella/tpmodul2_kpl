4295bfc27a53e52bc0d4bb97d6e02baa5c5cbd2b
riyandabella <riyandabella561@gmail.com>
2025-02-23 15:09:36 +0700

    update 23/02/25

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// soal A
+/*Console.WriteLine("Masukkan satu huruf: ");
+char huruf = (char)Console.Read();
+if (huruf == 'A' || huruf == 'I' || huruf == 'U' || huruf == 'E' || huruf == 'O'
+    || huruf == 'a' || huruf == 'i' || huruf == 'u' || huruf == 'e' || huruf == 'o')
+{
+    Console.WriteLine("Huruf " + huruf + " merupakan huruf vokal.");
+}
+else
+{
+    Console.WriteLine("Huruf " + huruf + " merupakan huruf konsonan.");
+}*/
+
+//Soal B
+int[] genap = { 2, 4, 6, 8, 10 };
+for (int i = 0; i < genap.Length; i++)
+{
+    Console.WriteLine("Angka Genap " + (i + 1) + " : " + genap[i]);
+}
